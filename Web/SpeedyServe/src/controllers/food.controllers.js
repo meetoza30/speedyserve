220b6644b0a157cd565ec30709517daca1d6fe31
meetoza30 <meetoz305@gmail.com>
2025-01-30 22:40:21 +0530

    Removed file uploading

--- Web/SpeedyServe/src/controllers/food.controllers.js
+++ Web/SpeedyServe/src/controllers/food.controllers.js
@@ -3,7 +3,7 @@ import fs from "fs";
 
 const addFood = async(req,res)=>{
 
-    let image_filename = `${req.file.filename}`;
+    
 
     const food = new foodModel({
         name : req.body.name,
@@ -38,7 +38,7 @@ const removeFood = async(req,res)=>{
         try{
             const food = await foodModel.findById(req.body.id)
             // console.log(food)
-            fs.unlink(`uploads/${food.image}`,()=>{})
+            // fs.unlink(`uploads/${food.image}`,()=>{})
 
             await foodModel.findByIdAndDelete(req.body.id);
             res.json({success:true,message:"Food Removed"})
